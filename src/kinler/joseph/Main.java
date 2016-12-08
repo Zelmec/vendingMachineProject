af9b6fa5214dc99248779f1d2a536bcbb00077d0
Zelmec <kinlerj@gmail.com>
2016-12-07 20:36:02 -0600

    Added Static String to Factory Class

--- src/kinler/joseph/Main.java
+++ src/kinler/joseph/Main.java
@@ -1,8 +1,13 @@
 package kinler.joseph;
 
+import kinler.joseph.factories.VendingMachineFactory;
+import kinler.joseph.vendingMachines.SnackMachine;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        SnackMachine snackMachine = (SnackMachine) VendingMachineFactory.createVendingMachine(VendingMachineFactory.SNACK_MACHINE);
+
+        System.out.print("Test");
     }
 }
--- src/kinler/joseph/factories/VendingMachineFactory.java
+++ src/kinler/joseph/factories/VendingMachineFactory.java
@@ -7,12 +7,15 @@ import kinler.joseph.vendingMachines.VendingMachine;
  * Created by Kinle on 12/7/2016.
  */
 public class VendingMachineFactory {
+
+    public static final String SNACK_MACHINE = "SnackMachine";
+
     public static VendingMachine createVendingMachine(String machineType){
         if(machineType == null){
             return null;
         }
 
-        if(machineType.equalsIgnoreCase("SnackMachine")){
+        if(machineType.equals(SNACK_MACHINE)){
             return new SnackMachine();
         }
         return null;
