ad1799edf3e46a22d0c59ae4326138b56103df57
Zelmec <kinlerj@gmail.com>
2016-12-08 05:23:11 -0600

    Added prompt to get user snack selection.

--- src/kinler/joseph/Main.java
+++ src/kinler/joseph/Main.java
@@ -1,13 +1,62 @@
 package kinler.joseph;
 
+import kinler.joseph.enumTypes.Coin;
+import kinler.joseph.enumTypes.Snack;
 import kinler.joseph.factories.VendingMachineFactory;
 import kinler.joseph.vendingMachines.SnackMachine;
 
+import java.util.Scanner;
+
 public class Main {
+
+    private static final Scanner INPUT_SCANNER = new Scanner(System.in);
 
     public static void main(String[] args) {
         SnackMachine snackMachine = (SnackMachine) VendingMachineFactory.createVendingMachine(VendingMachineFactory.SNACK_MACHINE);
+        boolean isRunning = true;
 
-        System.out.print("Test");
+        while(isRunning){
+
+            snackMachine.selectItem(selectSnackPrompt());
+            isRunning = false;
+        }
+
+        INPUT_SCANNER.close();
+    }
+
+    private static Snack selectSnackPrompt(){
+        int selection;
+
+        System.out.println("Please select a snack!");
+        System.out.println("1. Chips");
+        System.out.println("2. Candy");
+        System.out.println("3. Cookies");
+        System.out.println("4. Crackers");
+        System.out.println();
+
+        do {
+            String input = INPUT_SCANNER.nextLine();
+
+            try{
+                selection = Integer.parseInt(input);
+
+                if(selection > 0 && selection <= Snack.values().length){
+                    return Snack.getSnackFromValue(selection);
+                } else{
+                    System.out.println("The selection you made was not valid. Please enter a different number");
+                    System.out.println();
+                }
+            } catch(NumberFormatException e){
+                System.out.println("NumberFormatException: input string \""+input+"\" could not be parsed to an integer.");
+                System.out.println();
+            }
+        } while(true);
+    }
+
+    private static Coin insertCoinPrompt(int price, String priceString){
+        int selection;
+
+        //TODO: Allow user to insert multiple coins
+        return null;
     }
 }
--- src/kinler/joseph/enumTypes/Snack.java
+++ src/kinler/joseph/enumTypes/Snack.java
@@ -13,6 +13,22 @@ public enum Snack {
         this.mPrice = price;
     }
 
+    //Return the snack corresponding to the given value, or null.
+    public static Snack getSnackFromValue(int value){
+        switch (value){
+            case 1:
+                return Snack.CHIPS;
+            case 2:
+                return Snack.CANDY;
+            case 3:
+                return Snack.COOKIES;
+            case 4:
+                return Snack.CRACKERS;
+            default:
+                return null;
+        }
+    }
+
     //Return the name of the snack
     public String getName(){
         return mName;
@@ -21,5 +37,13 @@ public enum Snack {
     //Return the price of the snack
     public int getPrice(){
         return mPrice;
+    }
+
+    //Return the price as a string with a decimal point inserted into it.
+    public String getPriceString(){
+        String priceString = Integer.toString(mPrice);
+        int length = priceString.length();
+
+        return priceString.substring(0, length - 2) + "." + priceString.substring(length - 2, length);
     }
 }
